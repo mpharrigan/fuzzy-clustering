f60adc58d01195bb4d32d6d0c7354c4cec077693
Matthew Harrigan <harrigan@stanford.edu>
2013-08-29 15:57:38 -0700

    Fixed grave errors
    It still doesn't actually do anything / work

--- fuzzy/validate.py
+++ fuzzy/validate.py
@@ -5,21 +5,21 @@ from fuzzy import analysis, classic, mixture
 import msmtoys.analytic
 import pickle
 
-def validate(tmatrix_fn):
+def validate(tmatrix_fn, num_trajs, traj_len, stride, n_timescales):
     with open(tmatrix_fn, 'rb') as tmatrix_f:
         t_matrix, grid = pickle.load(tmatrix_f)
-        
+
     vd = ValidationData()
-        
-    print "Aggregate: {:,} points".format((num_trajs*traj_len/stride))
+
+    print "Aggregate: {:,} points".format((num_trajs * traj_len / stride))
     traj_list = msmtoys.analytic.get_trajlist(t_matrix, grid, num_trajs, traj_len, stride)
-    
+
     analytic_its = analysis.get_implied_timescales(t_matrix, n_timescales=n_timescales, lag_time=1)
-    
-    
-    
+
+
+
 class ValidationData:
     """Container for all the stuffs."""
-    
+
     def __init__(self):
         self.analytic_its = None
